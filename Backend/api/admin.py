04c1f6a278ec9dc72118abeb7428f5cc735b601b
Zelalem <zzele57@gmail.com>
2025-04-05 15:16:09 +0300

    edited

--- Backend/api/admin.py
+++ Backend/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Farmer, Product, Order
+
+admin.site.register(Farmer)
+admin.site.register(Product)
+admin.site.register(Order)
